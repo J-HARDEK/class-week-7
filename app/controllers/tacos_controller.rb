1302fa7761b5d5f60df6f107f2eb788243687286
John Hardek <john.hardek@kellogg.northwestern.edu>
2025-02-20 03:01:00 +0000

    class

--- app/controllers/tacos_controller.rb
+++ app/controllers/tacos_controller.rb
@@ -1,7 +1,12 @@
 class TacosController < ApplicationController
 
   def index
+    @fillings = ["Carnitas", "Al Pastor", 
+    "Steak", "Fish", "Veggie"]
+    
     # render :template => "tacos/index"
+
+
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources "companies"
   # get("/companies", {:controller => "companies", :action => "index"})
+  
+ 
   resources "contacts"
   # get("/contacts", {:controller => "contacts", :action => "index"})
 
